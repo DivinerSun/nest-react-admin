61ebf6e3defb16dc58da546188fff37d5b96029f
Diviner <sqq_job@sina.com>
2023-03-03 20:27:41 +0800

    feat: 替换组件式路由

--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -1,23 +1,47 @@
 import { FC } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Navigate, RouteObject, useRoutes } from "react-router-dom";
 import Layout from "../layout";
 import { lazyLoad } from "./LazyLoad";
 
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Navigate to={"/home"} />,
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/home",
+        element: lazyLoad("home"),
+      },
+      {
+        path: "/system/user",
+        element: lazyLoad("system/user"),
+      },
+      {
+        path: "/system/role",
+        element: lazyLoad("system/role"),
+      },
+      {
+        path: "*",
+        element: lazyLoad("error"),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: lazyLoad("login"),
+  },
+  {
+    path: "*",
+    element: lazyLoad("error"),
+  },
+];
+
 const AppRouter: FC = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Navigate to={"/home"} />} />
-        <Route path="/" element={<Layout />}>
-          <Route path="/home" element={lazyLoad("home")} />
-          <Route path="/system/user" element={lazyLoad("user")} />
-          <Route path="/system/role" element={lazyLoad("role")} />
-        </Route>
-        <Route path="/login" element={lazyLoad("login")} />
-        <Route path="*" element={lazyLoad("error")} />
-      </Routes>
-    </>
-  );
+  return <>{useRoutes(routes)}</>;
 };
 
 export default AppRouter;
