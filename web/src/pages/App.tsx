af9f8029dd7c6709fde528cb5a0df3300ef7136d
Diviner <sqq_job@sina.com>
2024-07-30 14:20:55 +0800

    feat: 前端-项目初始化并配置代码格式化风格

---
+++ web/src/pages/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <p>Hello Vite + React!</p>
+      <p>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </p>
+    </div>
+  )
+}
+
+export default App
