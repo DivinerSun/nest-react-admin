9f0538cf342e3d1b602798a58654bc9c020c7166
Diviner <sqq_job@sina.com>
2024-07-30 14:44:25 +0800

    feat: 前端-配置项目路径别名和开发环境信息

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { join } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': join(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8088/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
